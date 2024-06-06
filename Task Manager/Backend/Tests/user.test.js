e9ca7cb7f2c014e2f1dc83495c08e18dcc0b3b20
Rahul Mahato <mahatorahul2901@gmail.com>
2024-06-06 09:52:57 +0530

    added test case for mail-sending and file-upload

--- Task Manager/Backend/Tests/user.test.js
+++ Task Manager/Backend/Tests/user.test.js
@@ -20,20 +20,39 @@ beforeEach(async () => {
     await new User(userOne).save();   //this is for checking login-credentials
 })
 
+//eveytime it is testing the registeration, it is sending a mail to a unknow user-rahu123@gmail, and if we are using any paid subscription for sending mail, the it will coast us, 
+//so to avoid this, we are using mock-library to stop sending mail during the testing.
 test('Register-User', async () => {
-    await request(app).post('/user/registerUsers').send({
+    const response = await request(app).post('/user/registerUsers').send({
         "name": "Rahul Mahato",
         "email": "rahul123@gmail.com",     //email should be unique, then only it will work - according to userShcema (to resolve this issue - we will use "beforeEach() function")
         "password": "Rahul@123!",
         "age": 22
     }).expect(200)   //here 200 is the success status code.
+
+    // Assert that the database was changed correctly, ensure we get back the users details after registeration
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull();
+
+    // Assertions about the response
+    expect(response.body).toMatchObject({
+        user: {
+            name: 'Rahul Mahato',
+            email: 'rahul123@gmail.com'
+        },
+        token: user.tokens[0].token
+    })
+    expect(user.password).not.toBe('Rahul@123!')
 })
 
 test('Login-User', async () => {
-    await request(app).post('/user/login').send({
+    const response = await request(app).post('/user/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Not-Login-User', async () => {
@@ -72,6 +91,9 @@ test('Delete-User-Account-By-Id', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
 })
 
 test('Not-Delete-Unauthorized-User', async () => {
@@ -80,3 +102,11 @@ test('Not-Delete-Unauthorized-User', async () => {
         .send()
         .expect(400)
 })
+
+// test('Upload-profile-picture', async () => {
+//     await request(app)
+//         .post(`/user/UpdateProfileImg/${userOneId}`)
+//         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+//         .attach('avatar', './Tests/fixtures/DataModel-Todo.jpg')
+//         .expect(200)
+// })
